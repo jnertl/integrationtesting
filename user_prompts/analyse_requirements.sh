26ab619fc62804b96f596c9a5ce41465cea66e2e
jne <jnertl@outlook.com>
2025-10-03 13:34:32 +0300

    Remove unused mcpServers configuration from analyse_requirements.sh

--- user_prompts/analyse_requirements.sh
+++ user_prompts/analyse_requirements.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env -S mcphost script
 ---
-mcpServers:
-  filesystem:
-    type: "builtin"
-    name: "fs"
-    options:
-      allowed_directories: [${env://SOURCE_ROOT_DIR}]
-      max_results: 100000
-
 model: "${env://MODEL:-ollama:llama3.2:3b}"
 ---
 Integration testing requirements are as follows:
