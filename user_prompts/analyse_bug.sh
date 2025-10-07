7c21f0070326f67e220014fdcbc9c9f6dc85b68f
jne <jnertl@outlook.com>
2025-10-07 15:12:40 +0300

    Update quality_manager.txt and analyse_bug.sh for clarity and variable adjustments

--- user_prompts/analyse_bug.sh
+++ user_prompts/analyse_bug.sh
@@ -5,18 +5,8 @@ mcpServers:
     type: "local"
     command: ["npx", "-y", "@modelcontextprotocol/server-filesystem", "${env://SOURCE_ROOT_DIR}"]
 
-model: "${env://MODEL:-ollama:granite3.1-moe:3b}"
+model: "${env://MODEL}"
 ---
-Integration testing requirements are as follows:
-${env://TEST_REQUIREMENTS}
-
-Middlewaresw source code is in directory: [${env://MIDDLEWARE_SOURCE_CODE}]
-
-GUI client source code is in directory: [${env://GUI_CLIENT_SOURCE_CODE}]
-
-Test case source code is in directory: [${env://TEST_SOURCE_CODE}]
-
-Test results files are in directory: [${env://TEST_RESULTS_FOLDER_FOR_AI}]
-
 My question is:
 ${env://AI_PROMPT}
+
